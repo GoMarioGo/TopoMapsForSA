c730d8cd2cf69113cead7faf822349f20eb0a222
ToninoTarsi <tony.tarsi@gmail.com>
2016-09-06 09:18:10 +0200

    1.00

---
+++ GIS2LKMAP/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace GIS2LKMAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonASC2LK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonLK2ASC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonASC2LK
+            // 
+            this.buttonASC2LK.Location = new System.Drawing.Point(109, 150);
+            this.buttonASC2LK.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonASC2LK.Name = "buttonASC2LK";
+            this.buttonASC2LK.Size = new System.Drawing.Size(107, 55);
+            this.buttonASC2LK.TabIndex = 1;
+            this.buttonASC2LK.Text = "ASC to KMAP ";
+            this.buttonASC2LK.UseVisualStyleBackColor = true;
+            this.buttonASC2LK.Click += new System.EventHandler(this.buttonASC2LK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonLK2ASC
+            // 
+            this.buttonLK2ASC.Location = new System.Drawing.Point(109, 53);
+            this.buttonLK2ASC.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonLK2ASC.Name = "buttonLK2ASC";
+            this.buttonLK2ASC.Size = new System.Drawing.Size(107, 50);
+            this.buttonLK2ASC.TabIndex = 0;
+            this.buttonLK2ASC.Text = "LKMAP to ASC";
+            this.buttonLK2ASC.UseVisualStyleBackColor = true;
+            this.buttonLK2ASC.Click += new System.EventHandler(this.buttonLK2ASC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 277);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonASC2LK);
+            this.Controls.Add(this.buttonLK2ASC);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "GIS2LKMAP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonASC2LK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonLK2ASC;
+    }
+}
+
---
+++ GIS2LKMAP/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GIS2LKMAP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLK2ASC_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "LK8000 DEM files (*.dem)|*.dem";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String fileName = openFileDialog1.FileName;
+                String outFileAscName = Path.ChangeExtension(fileName, ".asc");
+
+                double Left;
+                double Right;
+                double Top;
+                double Bottom;
+                double StepSize;
+                uint Rows;
+                uint Columns;
+
+                if (File.Exists(fileName))
+                {
+                    using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                    {
+                        Left = reader.ReadDouble();
+                        Right = reader.ReadDouble();
+                        Top = reader.ReadDouble();
+                        Bottom = reader.ReadDouble();
+                        StepSize = reader.ReadDouble();
+                        Rows = reader.ReadUInt32();
+                        Columns = reader.ReadUInt32();
+
+                        //uint nsize = Rows * Columns;
+
+                        var utf8WithoutBom = new System.Text.UTF8Encoding(false);
+                        FileStream ostream = new FileStream(outFileAscName, FileMode.Create, FileAccess.ReadWrite);
+                        StreamWriter writer = new StreamWriter(ostream, utf8WithoutBom);
+
+
+                        writer.WriteLine("ncols         " + Columns.ToString());
+                        writer.WriteLine("nrows         " + Rows.ToString());
+                        writer.WriteLine("xllcorner     " + Left.ToString());
+                        writer.WriteLine("yllcorner     " + Bottom.ToString());
+                        writer.WriteLine("cellsize      " + StepSize.ToString());
+
+                        writer.WriteLine("NODATA_value  0");
+                        writer.Flush();
+                        
+
+                        for (int r = 0; r < Rows; r++)
+                        {
+                            String line = "";
+                            short val;
+                            for (int c = 0; c < Columns; c++)
+                            {
+                                val = reader.ReadInt16();
+                                line += val.ToString() + " ";
+                            }
+                            writer.WriteLine(line);
+                        }
+
+                        writer.Flush();
+                        ostream.Close();
+
+                        MessageBox.Show("Done");
+                    }
+
+                    
+                }
+  
+            }
+        }
+
+        private void buttonASC2LK_Click(object sender, EventArgs e)
+        {
+           OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "LK8000 DEM files (*.asc)|*.asc";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String fileName = openFileDialog1.FileName;
+                String outFileDemName = Path.ChangeExtension(fileName, ".dem");
+
+                double Left=0;
+                double Right = 0;
+                double Top = 0;
+                double Bottom = 0;
+                double StepSize = 0;
+                uint Rows = 0;
+                uint Columns = 0;
+
+                if (File.Exists(fileName))
+                {
+                    BinaryWriter writer = new BinaryWriter(File.Open(outFileDemName, FileMode.Create));
+                    StreamReader sr = new StreamReader(fileName);
+                    for (int j = 0; j < 6; j++)
+                    {
+                        String line = sr.ReadLine();
+                        String[] components = line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                        switch  ( components[0] ) 
+                        {
+                            case "ncols":
+                                Columns = UInt16.Parse(components[1]);
+                                break;
+                            case "nrows":
+                                Rows = UInt16.Parse(components[1]);
+                                break;
+                            case "xllcorner":
+                                Left = Double.Parse(components[1]);
+                                break;
+                            case "yllcorner":
+                                Bottom = Double.Parse(components[1]);
+                                break;
+                            case "cellsize":
+                                StepSize = Double.Parse(components[1]);
+                                break;
+                        }
+
+                      
+
+                    }
+
+                    Top = Bottom + StepSize * (Rows);
+                    Right = Left + StepSize * (Columns );
+
+                    writer.Write(Left);
+                    writer.Write(Right);
+                    writer.Write(Top);
+                    writer.Write(Bottom);
+                    writer.Write(StepSize);
+                    writer.Write(Rows);
+                    writer.Write(Columns);
+
+
+                    for (int j = 0; j < Rows; j++)
+                    {
+                        String line = sr.ReadLine();
+                        String[] components = line.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                        for (int k = 0; k < Columns; k++)
+                        {
+                            short val = Int16.Parse(components[k]);
+                            if (val == -9999)
+                                val = 0;
+                            writer.Write((ushort)val);
+                        }
+
+                    }
+
+
+
+                    writer.Close();
+                    sr.Close();
+
+                    MessageBox.Show("Done");
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
